13ad4c44f26931496b22ed8527fabf0c2b4f0f4e
Sotatek-PhiNguyen <phi.nguyen@sotatek.com>
2019-03-08 11:14:52 +0700

    Hotfix: cannot prepare wallet balance, and throw error

--- src/callbacks/active/prepareWalletBalance.ts
+++ src/callbacks/active/prepareWalletBalance.ts
@@ -1,6 +1,6 @@
 import { Wallet, WalletBalance } from '../../entities';
 import { getConnection } from 'typeorm';
-import { Currency } from 'sota-common';
+import { Currency, Utils } from 'sota-common';
 
 export async function prepareWalletBalance(currency: Currency, symbol: string): Promise<void> {
   const connection = getConnection();
@@ -28,11 +28,21 @@ export async function prepareWalletBalanceAll(currency: Currency, symbols: strin
 
   const [wallets] = await Promise.all([connection.getRepository(Wallet).find({ currency })]);
 
-  const values: any = wallets.map(wallet => {
-    return symbols.map(symbol => ({
-      walletId: wallet.id,
-      coin: symbol,
-    }));
+  const values: any[] = [];
+
+  wallets.map(wallet => {
+    values.push(
+      ...symbols.map(symbol => ({
+        walletId: wallet.id,
+        coin: symbol,
+        balance: 0,
+        withdrawalPending: 0,
+        withdrawalTotal: 0,
+        depositTotal: 0,
+        createdAt: Utils.nowInSeconds(),
+        updatedAt: Utils.nowInSeconds(),
+      }))
+    );
   });
 
   await connection
@@ -40,7 +50,6 @@ export async function prepareWalletBalanceAll(currency: Currency, symbols: strin
     .insert()
     .into(WalletBalance)
     .values(values)
-    .orIgnore()
     .execute();
 
   return;
--- src/callbacks/crawl/onCrawlingTxs.ts
+++ src/callbacks/crawl/onCrawlingTxs.ts
@@ -24,6 +24,7 @@ export default async function onCrawlingTxs(crawler: BaseCrawler, allTxs: Transa
     } catch (e) {
       logger.error(`onCrawlingTxs failed with error`);
       logger.error(e);
+      throw e;
     }
   });
 }
