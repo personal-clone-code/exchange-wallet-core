84f21c65ead534495ee8745af46cb3d8dc5170c8
Sinh Vu <sinh.vu@sotatek.com>
2019-12-04 11:00:43 +0700

    add check withdrawal db tools

--- src/dbtools/doCheckWithdrawal.ts
+++ src/dbtools/doCheckWithdrawal.ts
@@ -1,6 +1,8 @@
 import * as _ from 'lodash';
 import { EntityManager, getConnection } from 'typeorm';
-import { getLogger } from 'sota-common';
+import { getLogger, Utils, GatewayRegistry, TransactionStatus } from 'sota-common';
+import { Withdrawal } from '../entities';
+import { WithdrawalStatus } from '../Enums';
 
 const limit = 50;
 const logger = getLogger('DBTools::# Withdrawal::');
@@ -15,7 +17,33 @@ async function _doCheckWithdrawal(manager: EntityManager): Promise<void> {
   logger.info(`>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>`);
   logger.info(`Start checking...`);
   const withdrawalErrors: string[] = [];
-  // TODO: implement me.
+  const totalInvalidWithdrawals = await manager
+    .getRepository(Withdrawal)
+    .createQueryBuilder()
+    .where(`status IN ('signing', 'signed', 'sent')`)
+    .andWhere(`updated_at < ${Utils.nowInMillis() - 30 * 60 * 60}`)
+    .getCount();
+  const totalRound = Math.ceil(totalInvalidWithdrawals / limit);
+  const round = Array.from(Array(totalRound).keys());
+  for (const r of round) {
+    const unCompletedWithdrawals: any[] = await manager
+      .getRepository(Withdrawal)
+      .createQueryBuilder()
+      .where(`status IN ('signing', 'signed', 'sent')`)
+      .andWhere(`updated_at < ${Utils.nowInMillis() - 30 * 60 * 60}`)
+      .take(limit)
+      .skip(r * limit)
+      .execute();
+    if (!unCompletedWithdrawals || unCompletedWithdrawals.length === 0) {
+      logger.warn(`There\'re no withdrawals. So, skip round ${r}.`);
+      return;
+    }
+    unCompletedWithdrawals.map(withdrawal => {
+      withdrawalErrors.push(
+        `Withdrawal id ${withdrawal.Withdrawal_id} in \'${withdrawal.Withdrawal_status}\' over 30 minutes.`
+      );
+    });
+  }
   logger.info(
     `${JSON.stringify({
       isOK: withdrawalErrors.length === 0,
