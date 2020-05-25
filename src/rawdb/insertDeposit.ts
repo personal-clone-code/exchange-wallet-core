4e8b98f7f4ecb8f93c6a7aac63016cdaaf0e84e0
CongNguyen <cong.nguyen@sotatek.com>
2020-05-25 17:14:22 +0700

    update logic for Ada

--- src/rawdb/insertDeposit.ts
+++ src/rawdb/insertDeposit.ts
@@ -71,7 +71,10 @@ export async function insertDeposit(
   // Persist deposit data in main table
   const depositId = (await manager.getRepository(Deposit).save(deposit)).id;
   await rawdb.insertDepositLog(manager, depositId, DepositEvent.CREATED, depositId, wallet.userId);
-  if (deposit.collectedTxid === 'NO_COLLECT_HOT_WALLET_ADDRESS') {
+  if (
+    deposit.collectedTxid === 'NO_COLLECT_HOT_WALLET_ADDRESS' ||
+    currencyInfo.platform === BlockchainPlatform.Cardano
+  ) {
     await rawdb.updateWalletBalanceAfterDeposit(manager, depositId, new BigNumber(deposit.amount));
   }
   return;
